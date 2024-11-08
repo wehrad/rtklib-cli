63a9c6f5cc7cb89df91409487eb0ea5e7a6d2de1
AdrienWehrle <adrien.wehrle@hotmail.fr>
2024-11-08 08:40:39 +0100

    use more standard paths

--- src/PPK_processing_pipeline.sh
+++ src/PPK_processing_pipeline.sh
@@ -1,10 +1,10 @@
 # convert UBX to RINEX 2.11
-./convert_GNSS.sh -i /scratch3/armin/work_adri/rover -e ubx -rv 2.11
-./convert_GNSS.sh -i /scratch3/armin/work_adri/base -e ubx -rv 2.11
+./convert_GNSS.sh -i /my/path/rover -e ubx -rv 2.11
+./convert_GNSS.sh -i /my/path/base -e ubx -rv 2.11
 
 # process rover data with base in Post-Processed Kinematics (PPK)
-./process_PPK.sh -p /home/guschti/RTKLIB\
-		 -b /scratch3/armin/work_adri/base\
-		 -r /scratch3/armin/work_adri/rover\
-		 -o /scratch3/armin/work_adri/PPK_results\
-		 -c /home/guschti/COEBELI/rtklib-cli/data/PPK.conf
+./process_PPK.sh -p /my/path/RTKLIB\
+		 -b /my/path/base\
+		 -r /my/path/rover\
+		 -o /my/path/PPK_results\
+		 -c /my/path/rtklib-cli/data/PPK.conf
